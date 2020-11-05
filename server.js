8999795afc5a90e1d7c9e86d619976abb3b9de47
Loïc Sikidi <loicsikidi@MacBook-Pro-de-Loic.local>
2020-11-06 00:34:58 +0100

    fix bug

--- server.js
+++ server.js
@@ -1,12 +1,15 @@
 const express   = require('express')
 const swaggerUI = require('swagger-ui-express')
 const yamljs    = require('yamljs')
+const path      = require('path')
 const PORT      = 3005
 const app       = express()
 
+const apiSpecPath = path.join(__dirname, 'openapi.yaml')
+
 app.disable('x-powered-by')
 
-app.get('*', swaggerUI.serve, swaggerUI.setup(yamljs.load('openapi.yml')))
+app.get('*', swaggerUI.serve, swaggerUI.setup(yamljs.load(apiSpecPath)))
 
 console.log(`The server listen on port: ${PORT}`)
 const server = app.listen(PORT)

5f77e1a18367a984dc6a5c0a4579553207bfc961
Loïc Sikidi <loicsikidi@MacBook-Pro-de-Loic.local>
2020-11-18 23:43:58 +0100

    Ajout de plusieurs doc

--- server.js
+++ server.js
@@ -6,10 +6,14 @@ const PORT      = 3005
 const app       = express()
 
 const apiSpecPath = path.join(__dirname, 'openapi.yaml')
+const exo1 = path.join(__dirname, 'soluce_exo1.yaml')
+const exo2 = path.join(__dirname, 'soluce_exo2.yaml')
 
 app.disable('x-powered-by')
+app.use("/default", swaggerUI.serve, (...args) => swaggerUI.setup(yamljs.load(apiSpecPath))(...args))
+app.use("/solution-exo-1", swaggerUI.serve, (...args) => swaggerUI.setup(yamljs.load(exo1))(...args))
+app.use("/solution-exo-2", swaggerUI.serve, (...args) => swaggerUI.setup(yamljs.load(exo2))(...args))
 
-app.get('*', swaggerUI.serve, swaggerUI.setup(yamljs.load(apiSpecPath)))
 
 console.log(`The server listen on port: ${PORT}`)
 const server = app.listen(PORT)

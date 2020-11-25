44cc3096da036f6a399a2a6a050e9c3b413257ae
Loïc Sikidi <loicsikidi@MacBook-Pro-de-Loic.local>
2020-11-25 08:04:37 +0100

    Ajout de faker

--- server.js
+++ server.js
@@ -1,23 +1,44 @@
-const express   = require('express')
-const swaggerUI = require('swagger-ui-express')
-const yamljs    = require('yamljs')
-const path      = require('path')
-const PORT      = 3005
-const app       = express()
-
-const equipes   = require('./src/routes/equipes')
-app.use('/api/v1/equipes', equipes)
+const express    = require('express')
+const bodyParser = require('body-parser');
+const equipes    = require('./src/routes/equipes')
+const swaggerUI  = require('swagger-ui-express')
+const yamljs     = require('yamljs')
+const path       = require('path')
+const app        = express()
+const PORT       = 3005
 
 /**
  * Pages de documentation
  */
-const apiSpecPath = path.join(__dirname, 'docs', 'openapi.yaml')
-const exo1 = path.join(__dirname, 'docs','soluce_exo1.yaml')
-const exo2 = path.join(__dirname, 'docs', 'soluce_exo2.yaml')
-app.use("/default", swaggerUI.serve, (...args) => swaggerUI.setup(yamljs.load(apiSpecPath))(...args))
-app.use("/solution-exo-1", swaggerUI.serve, (...args) => swaggerUI.setup(yamljs.load(exo1))(...args))
-app.use("/solution-exo-2", swaggerUI.serve, (...args) => swaggerUI.setup(yamljs.load(exo2))(...args))
+const getDocumentationPath = (filename) => path.join(__dirname, 'docs', `${filename}.yaml`)
+
+app.use(bodyParser.json())
 app.disable('x-powered-by')
+app.use("/default", swaggerUI.serve, (...args) => swaggerUI.setup(yamljs.load(getDocumentationPath("default")))(...args))
+app.use("/solution-exo-1", swaggerUI.serve, (...args) => swaggerUI.setup(yamljs.load(getDocumentationPath("soluce_exo1")))(...args))
+app.use("/solution-exo-2", swaggerUI.serve, (...args) => swaggerUI.setup(yamljs.load(getDocumentationPath("soluce_exo2")))(...args))
+
+
+// const OpenApiValidator = require('express-openapi-validator')
+// app.use(
+//     OpenApiValidator.middleware({
+//       apiSpec: './si-livedemo-api.yaml',
+//       validateResponses: true,
+//     }),
+// )
+
+app.use('/api/v1/equipes', equipes)
+
+/**
+ * ERROR HANDLERS
+ */
+
+app.use((err, req, res, next) => {
+    res.status(err.status || 500).json({
+      message: err.message,
+      errors: err.errors,
+    })
+})
 
 console.log(`The server listen on port: ${PORT}`)
 const server = app.listen(PORT)
--- src/routes/equipes.js
+++ src/routes/equipes.js
@@ -1,7 +1,42 @@
 const router = require('express').Router()
+const faker = require('faker')
+faker.locale = "fr"
 
-router.get('/', async (_, res) => {
-    res.send({msg: "hello worlds"})
+const NB_EQUIPES = 5
+
+router.get('/', async (req, res, next) => {
+    try {
+        const { pays } = req.query
+        const response = {foo: getEquipes(pays)}
+        res.send(response)
+    } catch (err) {
+        next(err)
+    }
 })
+
+const getEquipes = (pays) => {
+   const allEquipes = fakeDataset()
+   if(pays) {
+       return allEquipes.filter(equipe => equipe.pays == pays)
+   }
+   return allEquipes
+}
+
+const fakeDataset = () => {
+    equipes = []
+    for (let index = 0; index < NB_EQUIPES; index++) {
+        equipes.push({
+            id   : faker.random.uuid(),
+            nom  : faker.company.companyName(),
+            pays : faker.address.country(),
+        })
+    }
+    return equipes
+}
+
+/**
+ * Fonction utilitaire
+ */
+const uneChanceSurDeux = () => Math.random() <= 0.5
 
 module.exports = router
